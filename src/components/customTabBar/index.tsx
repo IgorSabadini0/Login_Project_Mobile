bead1865c397d242898a72d8b4eaf88f8221952c
IgorSabadini0 <igorsabadini157@gmail.com>
2026-05-20 20:39:31 -0300

    feat: return View in customTabBar

--- src/components/customTabBar/index.tsx
+++ src/components/customTabBar/index.tsx
@@ -2,3 +2,13 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { } from "@expo/vector-icons";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+
+const CustomTabBar: React.FC<BottomTabBarProps> = (props) => {
+    const { state, descriptors, navigation } = props;
+
+    return (
+        <View style={{ flexDirection: "row", height: 60, backgroundColor: "#fff" }}>
+            <TouchableOpacity></TouchableOpacity>
+        </View>
+    )
+}
