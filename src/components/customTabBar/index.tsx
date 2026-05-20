d76d625418f48368e004811ddcb89335c9de2b79
IgorSabadini0 <igorsabadini157@gmail.com>
2026-05-20 20:20:43 -0300

    feat: customTabbar

---
+++ src/components/customTabBar/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { } from "@expo/vector-icons";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
